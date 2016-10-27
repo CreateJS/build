f9b6721b9f7b85db1e232f6e4f780bd710cae78a
Patrick McGuckin <patrick@gskinner.com>
2016-10-27 10:17:07 -0600

    Moved Ticker static getters to regular properties.

--- src/utils/Ticker.js
+++ src/utils/Ticker.js
@@ -194,62 +194,6 @@ export default class Ticker extends EventDispatcher {
 		this._raf = true;
 	}
 
-// constants:
-	/**
-	 * In this mode, Ticker uses the requestAnimationFrame API, but attempts to synch the ticks to target framerate. It
-	 * uses a simple heuristic that compares the time of the RAF return to the target time for the current frame and
-	 * dispatches the tick when the time is within a certain threshold.
-	 *
-	 * This mode has a higher variance for time between frames than {{#crossLink "Ticker/TIMEOUT:property"}}{{/crossLink}},
-	 * but does not require that content be time based as with {{#crossLink "Ticker/RAF:property"}}{{/crossLink}} while
-	 * gaining the benefits of that API (screen synch, background throttling).
-	 *
-	 * Variance is usually lowest for framerates that are a divisor of the RAF frequency. This is usually 60, so
-	 * framerates of 10, 12, 15, 20, and 30 work well.
-	 *
-	 * Falls back to {{#crossLink "Ticker/TIMEOUT:property"}}{{/crossLink}} if the requestAnimationFrame API is not
-	 * supported.
-	 * @property RAF_SYNCHED
-	 * @static
-	 * @type {String}
-	 * @default "synched"
-	 * @readonly
-	 */
-	static get RAF_SYNCHED () {
-		return "synched";
-	}
-
-	/**
-	 * In this mode, Ticker passes through the requestAnimationFrame heartbeat, ignoring the target framerate completely.
-	 * Because requestAnimationFrame frequency is not deterministic, any content using this mode should be time based.
-	 * You can leverage {{#crossLink "Ticker/getTime"}}{{/crossLink}} and the {{#crossLink "Ticker/tick:event"}}{{/crossLink}}
-	 * event object's "delta" properties to make this easier.
-	 *
-	 * Falls back on {{#crossLink "Ticker/TIMEOUT:property"}}{{/crossLink}} if the requestAnimationFrame API is not
-	 * supported.
-	 * @property RAF
-	 * @static
-	 * @type {String}
-	 * @default "raf"
-	 * @readonly
-	 */
-	static get RAF () {
-		return "raf";
-	}
-
-	/**
-	 * In this mode, Ticker uses the setTimeout API. This provides predictable, adaptive frame timing, but does not
-	 * provide the benefits of requestAnimationFrame (screen synch, background throttling).
-	 * @property TIMEOUT
-	 * @static
-	 * @type {String}
-	 * @default "timeout"
-	 * @readonly
-	 */
-	static get TIMEOUT () {
-		return "timeout";
-	}
-
 // accessor properties:
 	/**
 	 * Indicates the target time (in milliseconds) between ticks. Default is 50 (20 FPS).
@@ -504,6 +448,56 @@ export default class Ticker extends EventDispatcher {
 	}
 
 }
+
+// constants:
+/**
+ * In this mode, Ticker uses the requestAnimationFrame API, but attempts to synch the ticks to target framerate. It
+ * uses a simple heuristic that compares the time of the RAF return to the target time for the current frame and
+ * dispatches the tick when the time is within a certain threshold.
+ *
+ * This mode has a higher variance for time between frames than {{#crossLink "Ticker/TIMEOUT:property"}}{{/crossLink}},
+ * but does not require that content be time based as with {{#crossLink "Ticker/RAF:property"}}{{/crossLink}} while
+ * gaining the benefits of that API (screen synch, background throttling).
+ *
+ * Variance is usually lowest for framerates that are a divisor of the RAF frequency. This is usually 60, so
+ * framerates of 10, 12, 15, 20, and 30 work well.
+ *
+ * Falls back to {{#crossLink "Ticker/TIMEOUT:property"}}{{/crossLink}} if the requestAnimationFrame API is not
+ * supported.
+ * @property RAF_SYNCHED
+ * @static
+ * @type {String}
+ * @default "synched"
+ * @readonly
+ */
+Ticker.RAF_SYNCHED = "synched";
+
+/**
+ * In this mode, Ticker passes through the requestAnimationFrame heartbeat, ignoring the target framerate completely.
+ * Because requestAnimationFrame frequency is not deterministic, any content using this mode should be time based.
+ * You can leverage {{#crossLink "Ticker/getTime"}}{{/crossLink}} and the {{#crossLink "Ticker/tick:event"}}{{/crossLink}}
+ * event object's "delta" properties to make this easier.
+ *
+ * Falls back on {{#crossLink "Ticker/TIMEOUT:property"}}{{/crossLink}} if the requestAnimationFrame API is not
+ * supported.
+ * @property RAF
+ * @static
+ * @type {String}
+ * @default "raf"
+ * @readonly
+ */
+Ticker.RAF = "raf";
+
+/**
+ * In this mode, Ticker uses the setTimeout API. This provides predictable, adaptive frame timing, but does not
+ * provide the benefits of requestAnimationFrame (screen synch, background throttling).
+ * @property TIMEOUT
+ * @static
+ * @type {String}
+ * @default "timeout"
+ * @readonly
+ */
+Ticker.TIMEOUT = "timeout";
 
 // events:
 /**
